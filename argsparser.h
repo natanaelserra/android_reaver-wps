8196fd5aa089520bcc1c8ea19aac8cd1a49d8f27
natanaelserra <aluno@pedagogico.CEUMA.EDU.BR>
2016-04-16 11:59:55 -0300

    nada

--- argsparser.h
+++ argsparser.h
@@ -48,7 +48,7 @@ extern int  REAVER_DEBUG;
 
 int process_arguments(int argc, char **argv);
 void init_default_settings(void);
-void parse_recurring_delay(char *arg);
+
 void parse_static_pin(char *pin);
 void process_auto_options(void);
 
